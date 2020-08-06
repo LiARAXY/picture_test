da391d492dfcc804b1eeed1319d77b382deb6b39
LiARAXY <liaraxy@outlook.com>
2020-08-06 23:17:21 +0800

    Update module_picture_plugin_zoom.c

--- src/module_picture_plugin_zoom.c
+++ src/module_picture_plugin_zoom.c
@@ -47,32 +47,32 @@ static int zoom_mode_1(p_picture_info info_src,p_picture_info info_dst, unsigned
         printf("ERROR : zoom data_src is invalid!\n");
         return -1;
     }
-    double m,n,x_tmp,y_tmp,fr1,fr2;
-    unsigned int i,j,x_src,y_src,x_dst,y_dst,x1,x2,y1,y2;
-    double f11,f12,f21,f22;
-    x_src = info_src->resX;
-    y_src = info_src->resY;
-    x_dst = info_dst->resX;
-    y_dst = info_dst->resY;
-    m = (double)x_dst/x_src;
-    n = (double)y_dst/y_src;
-    for (i = 0; i < y_dst; i++)
+    double m,n,x_tmp,y_tmp,f11,f12,f21,f22,tmpVal,help;
+    unsigned int xRes_src,yRes_src,xRes_dst,yRes_dst;
+    int i,j,x1,x2,y1,y2;
+    xRes_src = info_src->resX;
+    yRes_src = info_src->resY;
+    xRes_dst = info_dst->resX;
+    yRes_dst = info_dst->resY;
+    m = (double)xRes_dst/xRes_src;
+    n = (double)yRes_dst/yRes_src;
+    for (i = 0; i < yRes_dst; i++)
     {
-        for (j = 0; j < x_dst; j++)
+        for (j = 0; j < xRes_dst; j++)
         {
             x_tmp = j / m;
             y_tmp = i / n;
             x1 = floor(x_tmp);
             x2 = ceil(x_tmp);
-            y1 = floor(y_tmp);
-            y2 = ceil(y_tmp);
-            f11 = data_src[ y_src*x1 + y1];
-            f12 = data_src[ y_src*x1 + y2];
-            f21 = data_src[ y_src*x2 + y1];
-            f22 = data_src[ y_src*x2 + y2];
-            fr1 = f11*(x2*m - j)/((x2 - x1)*m) + f21*(j - x1*m)/((x2 - x1)*m);
-            fr2 = f12*(x2*m - j)/((x2 - x1)*m) + f22*(j - x1*m)/((x2 - x1)*m);
-            data_dst[j + i*x_dst] = round( ((double)(y2*n - i)/((y2 -y1)*n))*fr1 + ((double)(i - y1*n)/((y2 -y1)*n))*fr2 );
+            y1 = ceil(y_tmp);
+            y2 = floor(y_tmp);
+            help = m*n*(x2 - x1)*(y2 - y1);
+            f11 = data_src[ x1 + y1*xRes_src ];
+            f12 = data_src[ x1 + y2*xRes_src ];
+            f21 = data_src[ x2 + y1*xRes_src ];
+            f22 = data_src[ x2 + y2*xRes_src ];
+            tmpVal = ( f11*(j - x1*m)*(i - y1*n) + f12*(j - x1*m)*(y2*n - i) + f21*(x2*m - j)*(i - y1*n) + f22*(x2*m - j)*(y2*n - i) )/help;
+            data_dst[i*xRes_dst + j] = (unsigned int)round(tmpVal);
         }
     }
     return 0;
